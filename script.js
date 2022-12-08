6c1c4612ec6fd28d39b623e5ae9fb53739f62c5b
NyPadilha <47741746+NyPadilha@users.noreply.github.com>
2022-12-08 17:51:25 -0300

    I deleted the buttons to use the prompt

--- script.js
+++ script.js
@@ -16,20 +16,19 @@ function getComputerChoice(choice) {
 const computerChoice = getComputerChoice()
 
 function playRound(computerChoice, playerChoice) {
-    let winner = ""
     if(computerChoice == "rock" && playerChoice == "paper"){
-        winner = ("You Win, Paper beats Rock");
+        console.log("You Win, Paper beats Rock");
     }else if(computerChoice == "rock" && playerChoice == "scissors") {
-        winner = ("You Lose, Rock beats Scissors");
+        console.log("You Lose, Rock beats Scissors");
     }else if(computerChoice == "paper" && playerChoice == "scissors") {
-        winner = ("You Win, Scissors beats paper");
+        console.log("You Win, Scissors beats paper");
     }else if(playerChoice == "rock" && computerChoice == "paper"){
-        winner = ("You Lose, Paper beats Rock");
+        console.log("You Lose, Paper beats Rock");
     }else if(playerChoice == "rock" && computerChoice == "scissors") {
-        winner = ("You Win, Rock beats Scissors");
+        console.log("You Win, Rock beats Scissors");
     }else if(playerChoice == "paper" && computerChoice == "scissors") {
-        winner = ("You Lose, Scissors beats paper");
+        console.log("You Lose, Scissors beats paper");
     }else {
-        winner = ("its a tie");
+        console.log("its a tie");
     }
 }
